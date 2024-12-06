10139782c1920c9090642dfdf5e4d9fd08a01d53
DanielSolarte23 <royersolarte22@gmail.com>
2024-12-05 20:19:13 -0500

    Bien

--- loginDavid/src/pages/Login.jsx
+++ loginDavid/src/pages/Login.jsx
@@ -72,95 +72,11 @@ function Login() {
     return (
         <div className="bg-white font-dmSans flex font-semibold justify-center items-center h-screen">
             <article className="bg-white p-10 py-16 rounded-xl shadow-xl grid grid-cols-[300px_300px] min-w-[800px] justify-between relative overflow-hidden">
-                {/* Black Block */}
-                <div className={`absolute bottom-0 w-[200%] h-[200%] transition-all duration-[1s] blackBlock ${animationState.blackBlock}`}>
-                </div>
 
-                {/* Login Form */}
-                <form className={`grid gap-10 relative z-10 row-start-1 col-start-1 ${animationState.loginForm}`}>
-                    <h2 className="text-2xl font-bold after:block after:w-2/5 after:h-1 after:bg-verde-principal after:rounded-full after:mx-auto after:mt-1 text-center mb-2 text-zinc-600">
-                        Inicia Sesión
-                    </h2>
-
-                    <fieldset className="relative border-b-2 border-zinc-600 hover:border-verde-principal focus:border-verde-principal flex items-center">
-                        <input className="outline-none peer flex-1 bg-transparent h-full font-normal" type="text" placeholder="  " id="username" />
-                        <label className="absolute left-0 peer-focus:-translate-y-6 peer-focus:text-verde-principal transition-all text-zinc-600"
-                            htmlFor="username">Correo</label>
-                        <i className="bx bxs-user text-verde-principal"></i>
-                    </fieldset>
-
-                    <fieldset className="relative border-b-2 border-zinc-600 flex items-center transition-colors hover:border-verde-principal">
-                        <input className="outline-none peer flex-1 bg-transparent h-full" placeholder="  " type="password" id="password" />
-                        <label className="absolute left-0 peer-focus:-translate-y-6 peer-focus:text-verde-principal transition-all text-zinc-600"
-                            htmlFor="password">Contraseña</label>
-                        <i className="bx bxs-lock-alt text-verde-principal"></i>
-                    </fieldset>
-
-                    <button className="bg-verde-principal rounded-lg text-white py-2 hover:scale-105 transition-transform h-10">
-                        Iniciar Sesión
-                    </button>
-
-                    <span className="font-normal text-zinc-600">
-                        No tiene una cuenta?
-                        <button type="button" onClick={handleSignUpClick} className="font-bold text-verde-principal">
-                            Registrarse
-                        </button>
-                    </span>
-                </form>
-
-                {/* Right Text */}
-                <div className={`text-end max-w-[250px] ml-auto flex flex-col justify-center gap-4 text-white relative z-10 row-start-1 col-start-2 ${animationState.rightText}`}>
-                    <div className="object-contain rounded-full contain p-0">
-                        <img className="images" src={LogoArog} alt="" />
-                    </div>
-                </div>
-
-                {/* Left Text */}
-                <div className={`text-start max-w-[250px] mr-auto flex flex-col justify-center gap-4 text-white relative z-10 row-start-1 col-start-1 ${animationState.leftText}`}>
-                    <div className="object-contain">
-                        <img className="images" src={LogoArog} alt="" />
-                    </div>
-                </div>
-
-                {/* Sign Up Form */}
-                <form className={`grid gap-10 relative z-10 row-start-1 col-start-2 ${animationState.signUpForm}`}>
-                    <h2 className="text-2xl font-bold after:block after:w-2/5 after:h-1 after:bg-verde-principal after:rounded-full after:mx-auto after:mt-1 text-center mb-2">
-                        Registrarse
-                    </h2>
-
-                    <fieldset className="relative border-b-2 border-zinc-600 hover:border-verde-principal pb-2 flex items-center">
-                        <input className="outline-none peer flex-1 bg-transparent h-full font-normal" type="text" id="nombreCompleto" placeholder=" "/>
-                        <label className="absolute left-0 peer-focus:-translate-y-6 peer-focus:text-verde-principal transition-all"
-                            htmlFor="nombreCompleto">Nombre completo</label>
-                        <i className="bx bxs-user text-verde-principal"></i>
-                    </fieldset>
-
-                    <fieldset className="relative border-b-2 border-zinc-600 hover:border-verde-principal pb-2 flex items-center">
-                        <input className="outline-none peer flex-1 bg-transparent font-normal" type="email" id="emailRegistro" placeholder=" "/>
-                        <label className="absolute left-0 peer-focus:-translate-y-6 peer-focus:text-verde-principal transition-all"
-                            htmlFor="emailRegistro">Correo</label>
-                        <i className="bx bxs-user text-verde-principal"></i>
-                    </fieldset>
-
-                    <fieldset className="relative border-b-2 border-zinc-600 hover:border-verde-principal pb-2 flex items-center">
-                        <input className="outline-none peer flex-1 bg-transparent font-normal" type="password" id="passwordRegister" placeholder="  "/>
-                        <label className="absolute left-0 peer-focus:-translate-y-6 peer-focus:text-verde-principal transition-all"
-                            htmlFor="passwordRegister">Contraseña</label>
-                        <i className="bx bxs-lock-alt text-verde-principal"></i>
-                    </fieldset>
-
-                    <button className="bg-verde-principal rounded-lg text-white h-10 py-2 hover:scale-105 transition-transform">
-                        Registrarse
-                    </button>
-
-                    <span className="font-normal">
-                        Ya tienes una cuenta?
-                        <button type="button" onClick={handleLoginClick} className="text-verde-principal font-bold">
-                            Inicia Sesión
-                        </button>
-                    </span>
-                </form>
+                <div className={`absolute bottom-0 w-[200%] h-[200%] transition-all duration-[1s] blackBlock ${animationState.blackBlock}`}></div>
             </article>
+            
+
         </div>
     );
 }
